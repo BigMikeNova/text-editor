3d2f5b86bccb6cfee5e9f3f0f80b9b3a5f1226b6
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-07-10 20:37:33 -0400

    moved gitignore

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,5 +26,45 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset caching using StaleWhileRevalidate strategy
+const assetsCache = new StaleWhileRevalidate({
+  cacheName: 'assets-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+});
+
+registerRoute(({ request }) => request.destination === 'script' || request.destination === 'style', assetsCache);
+
+// Cache images using CacheFirst strategy
+const imagesCache = new CacheFirst({
+  cacheName: 'images-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+registerRoute(({ request }) => request.destination === 'image', imagesCache);
+
+// Cache API responses using CacheFirst strategy
+const apiCache = new CacheFirst({
+  cacheName: 'api-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 5 * 60, // Cache for 5 minutes
+    }),
+  ],
+});
+
+registerRoute('https://api.jate.com/', apiCache);
